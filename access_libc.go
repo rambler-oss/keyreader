71b306455c1eee98e36a32863bf28e3ed97a7fdc
Iavael <iavael@rambler-co.ru>
2017-01-20 20:26:07 +0300

    Fix nssInNetGr

--- access_libc.go
+++ access_libc.go
@@ -25,13 +25,34 @@ func (h Host) inNetGroups(netgroups []string) bool {
 	return false
 }
 
-func nssInNetGr(netgroup, host, user, domain *string) bool {
+func nssInNetGr(netgroup string, host, user, domain *string) bool {
+	var (
+		chost   *C.char
+		cuser   *C.char
+		cdomain *C.char
+	)
 	logger.Debug("Checking %s for membership in %s", host, netgroup)
-	cnetgr := C.CString(*netgroup)
+	cnetgr := C.CString(netgroup)
 	defer C.free(unsafe.Pointer(cnetgr))
-	chost := C.CString(*host)
-	defer C.free(unsafe.Pointer(chost))
-	if C.innetgr(cnetgr, chost, user, domain) > 0 {
+	if host != nil {
+		chost = C.CString(*host)
+		defer C.free(unsafe.Pointer(chost))
+	} else {
+		chost = nil
+	}
+	if user != nil {
+		cuser = C.CString(*user)
+		defer C.free(unsafe.Pointer(cuser))
+	} else {
+		cuser = nil
+	}
+	if domain != nil {
+		cdomain = C.CString(*domain)
+		defer C.free(unsafe.Pointer(cdomain))
+	} else {
+		cdomain = nil
+	}
+	if C.innetgr(cnetgr, chost, cuser, cdomain) > 0 {
 		return true
 	}
 	return false
